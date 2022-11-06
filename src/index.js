c3269e7e9ac19fe9d9fffb46f0d1fe604dc1770d
Dewan parmar <dewan.parmar@prowesssoft.com>
2022-11-07 01:22:01 +0530

    code

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'remixicon/fonts/remixicon.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <Router>
+  <App />
+  </Router>
+    
+  </React.StrictMode>
+);
+
